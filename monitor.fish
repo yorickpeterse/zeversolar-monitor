b822406c47e513761018dfd7a4eb1028acec8291
Yorick Peterse <yorick@yorickpeterse.com>
2024-09-18 10:43:23 +0200

    Ignore zero values from the inverter

--- monitor.fish
+++ monitor.fish
@@ -24,14 +24,20 @@ while true
         | tail -3 | head -1
     )
 
-    if [ "$raw" != "" ]
+    if test "$raw" = ""
+        echo 'The inverter is offline'
+    else if test $raw -eq 0
+        # Every now and then the inverter will randomly report the value to be
+        # zero in the middle of the day. Such values mess up with any
+        # dashboards, so we ignore them. It's also just a waste to log zero
+        # values.
+        echo 'The reported usage is zero, ignoring'
+    else
         # The raw data is in Kilowatt/hour.
         set produced (math "$raw * 1000")
 
         echo "inverter produced=$produced" | ncat --udp $DB_IP $DB_PORT
         echo "$produced Watts (raw: $raw kW)"
-    else
-        echo 'The inverter is offline'
     end
 
     sleep 1800
